f9250a7519cd0979450bdc3cda2f2290ac18d601
ashenmoon <85771856+ashenmoon@users.noreply.github.com>
2021-12-20 21:15:03 -0500

    Update Clock.java

--- Jeopardy/Clock.java
+++ Jeopardy/Clock.java
@@ -3,9 +3,9 @@ import hsa.*;
 import java.lang.*;
 
 public class Clock implements Runnable {
-    private static Console clockCon;
-    public static int time;
-    private boolean running;
+    private Console clockCon;
+    public int time;
+    private boolean running = true;
 
     public Clock () {
         running = true;
@@ -13,19 +13,18 @@ public class Clock implements Runnable {
 
     public void clockRun () {
         time = 30;
-        clockCon = new Console (20, 40);
+        clockCon = new Console (16, 40);
         clockCon.setColor (new Color (1, 55, 99));
         clockCon.fillRect (10, 10, 390, 390);
         clockCon.setFont (new Font ("Monospaced", Font.PLAIN, 200));
         while (time >= 0 && running) {
             //erasure comes before
             clockCon.setColor (new Color (1, 55, 99));
-            clockCon.drawString (time + 1 + "", 40, 230);
-
+            clockCon.drawString (time + 1 + "", 45, 220);
             //drawing stuff
             try {
                 clockCon.setColor (Color.white);
-                clockCon.drawString (time + "", 40, 230);
+                clockCon.drawString (time + "", 45, 220);
                 Thread.sleep (1000);
                 
                 time--;
